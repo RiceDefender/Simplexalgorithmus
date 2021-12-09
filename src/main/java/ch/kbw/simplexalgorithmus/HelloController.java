039748477998219685b77c6cdbf7cf03898f8983
Nexterbyby <jenseichenberger@gmail.com>
2021-12-09 11:39:04 +0100

    Make algorithm, failure

--- src/main/java/ch/kbw/simplexalgorithmus/HelloController.java
+++ src/main/java/ch/kbw/simplexalgorithmus/HelloController.java
@@ -5,13 +5,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    private double [][] ex_table = {
+            {2,2,1,0,0,16},
+            {4,2,0,1,0,24},
+            {4,6,0,0,1,36},
+            {-80,-60,0,0,0,0}
+    };
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-        PivotTable pt = new PivotTable(2, 3);
-        System.out.println(pt.toString());
+        PivotTable pt = new PivotTable();
+        pt.setPivotTable(ex_table);
+        pt.cycle();
+        pt.cycle();
+
     }
 }
--- src/main/java/ch/kbw/simplexalgorithmus/model/PivotTable.java
+++ src/main/java/ch/kbw/simplexalgorithmus/model/PivotTable.java
@@ -3,14 +3,47 @@ package ch.kbw.simplexalgorithmus.model;
 import java.util.Arrays;
 
 public class PivotTable {
-    int height;
-    int varCount;
-    double [][] pivotTable;
+    private double [][] pivotTable;
 
     public PivotTable(int varCount, int height){
-        this.varCount = varCount;
-        this.height = height;
-        this.pivotTable = new double[height + 1][varCount + height + 2];
+        this.pivotTable = new double[height + 1][varCount + height + 1];
+    }
+
+    public PivotTable(){
+        // nüscht
+    }
+
+    public void cycle(){
+        // vars
+        double [] temp = new double[pivotTable.length-1]; // will save the divisional result of "num pivot column" / "result of equation"
+        int indexX = findMin(pivotTable[pivotTable.length-1]); // is the pivot row
+        double pivotnum;
+
+
+        // find the pivot number
+        for(int i = 0; i < pivotTable.length -1; i++){
+            temp[i] = pivotTable[i][pivotTable[i].length-1] / pivotTable[i][indexX];
+        }
+        int indexY = findMin(temp);
+        pivotnum = pivotTable[indexY][indexX];
+        System.out.println(toString());
+        System.out.println(indexX + " : " + indexY + " : " + pivotnum);
+
+
+        // make pivot number 1 and ratio the rest of the pivot row
+        ratioRow(indexY, pivotnum);
+        System.out.println(toString());
+        for(int i = 0; i < pivotTable.length-1; i++){
+            ratioRow(i, pivotTable[i][indexX]);
+        }
+        System.out.println(toString());
+
+        // make every number in pivotCol except pivot num 0
+        for(int i = 0; i < pivotTable.length; i++){
+            if (i != indexY) subtractAgainst(i, indexY);
+        }
+        System.out.println(toString());
+
     }
 
     public String toString() {
@@ -35,5 +68,29 @@ public class PivotTable {
 
     public void setPivotTable(double[][] pivotTable) {
         this.pivotTable = pivotTable;
+    }
+
+    private int findMin(double [] da){
+        double min = da[0];
+        int index = 0;
+        for(int i = 0; i < da.length; i++){
+            if(da[i] < min){
+                min = da[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private void ratioRow(int rowIndex, double factor){
+        for(int i = 0; i < pivotTable[rowIndex].length; i++){
+            this.pivotTable[rowIndex][i] /= factor;
+        }
+    }
+
+    private void subtractAgainst(int rowI, int pivotRow) {
+        for(int i = 0; i < pivotTable[0].length; i++){
+            pivotTable[rowI][i] -= pivotTable[pivotRow][i];
+        }
     }
 }
