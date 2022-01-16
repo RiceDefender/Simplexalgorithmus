e47f70650784a8f23a8afb8cf48ebd15abf8d647
Nghia Dao <nghiadao2002@gmail.com>
2022-01-16 17:39:22 +0100

    Simplex Algorithmus v1.0

--- src/main/java/ch/kbw/simplexalgorithmus/HelloController.java
+++ src/main/java/ch/kbw/simplexalgorithmus/HelloController.java
@@ -6,16 +6,52 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     private double [][] ex_table = {
-            /*
+
             {2,2,1,0,0,16},
             {4,2,0,1,0,24},
             {4,6,0,0,1,36},
             {-80,-60,0,0,0,0}
+
+            /*
+            {1,1,1,0,0,90},
+            {10,5,0,1,0,800},
+            {3,6,0,0,1,420},
+            {-36,-45,0,0,0,0}
+
              */
+
+
+
+
+            /*
+            {1,1,1,0,0,0,0,90},
+            {10,5,0,1,0,0,0,800},
+            {3,6,0,0,1,0,0,420},
+            {-1,0,0,0,0,1,0,0},
+            {0,-1,0,0,0,0,1,0},
+            {36,45,0,0,0,0,0,0}
+
+             */
+            /*
+
+            {1,1,1,1,1,0,0,0},
+            { -1,2,0,0,0,1,0,0},
+            {2,10,10,2,0,0,1,0},
+            {2,3,3.5,4,0,0,0,1}
+            */
+            /*
             {4,4,1,0,0,32},
             {8,4,0,1,0,48},
             {8,12,0,0,1,72},
             {-160,-120,0,0,0,0}
+             */
+            /*
+            {1,1,1,1,1,0,0,0},
+            {4,2,0,1,0,24},
+            {4,6,0,0,1,36},
+            {-80,-60,0,0,0,0}
+             */
+
     };
 
     @FXML
@@ -24,7 +60,7 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
-        PivotTable pt = new PivotTable();
+        PivotTable pt = new PivotTable(2,3);
         pt.setPivotTable(ex_table);
         pt.cycle();
 
